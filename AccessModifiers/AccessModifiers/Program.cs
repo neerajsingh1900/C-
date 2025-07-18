db5cd00319878c131ad97bb4edfe460a7b8bb98d
neerajsingh1900 <neeraj.singh@coditas.com>
2025-07-18 10:11:31 +0530

    Add files via upload

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using LinkingFile;
+class Modifier: LinkingFile.Class1
+{
+    public static void Show()
+    {
+
+        Class1.DoubleChange(out int g);
+        Console.WriteLine(g);
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        LinkingFile.Class1 l = new LinkingFile.Class1();
+        l.SetTax(tax:78);
+        l.SetTotal(65);
+        LinkingFile.Class1 nn = new LinkingFile.Class1();
+        nn.SetTax(tax:33);
+        nn.SetTotal(87);
+
+
+
+        Console.WriteLine(LinkingFile.Class1.All(l, nn));
+
+
+        double q = 89.98;
+        Console.WriteLine(q);
+
+        Class1 cll = new Class1();
+        cll.change( percentage: ref q);
+        Console.WriteLine(q);
+
+        Modifier.Show();
+
+
+    }
+}
---
+++ AccessModifiers/LinkingFile/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkingFile
+{
+    public class Class1
+    {
+        private int tax;
+        private int total;
+
+        public void SetTax(int tax=0)
+        {
+            this.tax = tax;
+        }
+
+        public void SetTotal(int tx)
+        {
+            this.total = 7 * tx;
+        }
+
+        public int GetTax()
+        {
+            return 6*this.tax;
+        }
+        public int GetTotal()
+        {
+            return 7*this.total;
+        }
+
+        public static int All(Class1 c1, Class1 c2)
+        {
+            return c1.GetTotal() + c2.GetTotal();
+        }
+
+        public void change(ref double percentage)
+        {
+            percentage = 90.89;
+        }
+
+        protected internal static void DoubleChange(out int hell)
+        {
+            hell = 87;
+        }
+    }
+}
