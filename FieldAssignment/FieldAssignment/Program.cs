7e35f4e1439f5ba0464b635daf465b444cf18235
neerajsingh1900 <neeraj.singh@coditas.com>
2025-07-18 10:12:36 +0530

    Add files via upload

---
+++ FieldAssignment/FieldAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee e = new Employee(1,"Neeraj");
+        Employee e1 = new Employee(2, "Sumit");
+        Employee e2 = new Employee(3, "Rohit");
+        Employee e3 = new Employee(4, "Rohan");
+
+       System.Console.WriteLine(e.getId());
+        System.Console.WriteLine(e1.getId());
+        System.Console.WriteLine(e2.getId());
+        System.Console.WriteLine(e3.getId()); 
+        
+
+        System.Console.WriteLine(e.getName());
+        System.Console.WriteLine(e1.getName());
+        System.Console.WriteLine(e2.getName()); 
+        System.Console.WriteLine(e3.getName()); 
+        
+
+    }
+}
---
+++ FieldAssignment/HarshaIncLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Employee
+{
+    private string name;
+    private int id;
+
+   public  Employee(int id,string name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+    public string getName()
+    {
+        return name;
+    }
+}
