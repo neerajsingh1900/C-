c6016c414997e43fb5c611e66230ae3bd3a4adb1
neerajsingh1900 <neeraj.singh@coditas.com>
2025-07-18 14:12:57 +0530

    Add files via upload

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+public class Program
+{
+    //implicit casting
+    int a = 1;
+    long b;
+
+    //explicit casting
+    long c = 10;
+        
+    double bi = 99.99;      
+   
+
+
+    public void Change() {
+        b = a;
+    int k = (int)c;
+        System.Console.WriteLine(b + " " + c);
+
+        //explicit double to int
+        int e = (int)bi;
+        Console.WriteLine(e); 
+        
+    }
+    public static void Main(string[] args) { 
+    Program p = new Program();
+    p.Change();
+
+        string a = "done1";
+        string b = "done2";
+        string l = "bool";
+
+        //string to int
+        int m = int.Parse(a);
+        Console.WriteLine("provide an input");
+
+        //input of string to int 
+        bool k = int.TryParse(Console.ReadLine(), out int j);
+        Console.WriteLine(j);
+
+        //int to string 
+        int f = 1;
+        string s = Convert.ToString(f);
+        Console.WriteLine(s);
+
+        //string to int 
+        int y = Convert.ToInt32(s);
+        Console.WriteLine(y);
+
+        //double to float
+
+        double u = 89.79;
+
+        float t = (float)u;
+        Console.WriteLine(t);
+        }
+
+
+}
+
