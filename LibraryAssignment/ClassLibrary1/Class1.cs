6a03424aef209b4396ebe08ea6ec601dbb7f90a2
neerajsingh1900 <neeraj.singh@coditas.com>
2025-07-23 17:47:08 +0530

    Add files via upload

---
+++ LibraryAssignment/ClassLibrary1/Class1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+
+
+interface ILonable
+{
+     int LoanPeriod {  get; set; }
+     string Borrower {  get; set; }
+
+    void Borrow(string b);
+
+    void Return();
+}
+
+interface IPrintable
+{
+    void Print();
+}
+
+public class Book : ILonable, IPrintable
+{
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public string ISBN{ get; set; }
+    public string Borrower { get; set; }    
+    public int LoanPeriod { get; set; } 
+
+    public  void Borrow(string b)
+    {
+        if (Borrower == "")
+        {
+            Borrower = b;
+            System.Console.WriteLine($"Book is now Borrowed by: {Borrower}");
+
+        }
+        else
+        {
+            System.Console.WriteLine("Already the Book is Borrowed");
+        }
+    }
+
+    public void Return()
+    {
+        if (Borrower != "")
+        {
+            Borrower = "";
+            System.Console.WriteLine($"{Title} Book Returned Successfuly!");
+        }
+        else
+            System.Console.WriteLine("Book Not Borrowed");
+
+    }
+
+    public void Print()
+    {
+        System.Console.WriteLine($"Book {Title} of Author :{Author} is Borrowed by :{Borrower} for {LoanPeriod}");
+    }
+
+}
+
+public class DVD : IPrintable, ILonable
+{
+    public string Director { get; set; }
+    public string Title { get; set; }
+    public int LengthInMinutes { get; set; }
+
+    public int LoanPeriod { get; set; }
+
+    public string Borrower { get; set; }
+
+
+    public void Borrow(string b)
+    {
+        if (Borrower == "")
+        {
+            Borrower = b;
+            System.Console.WriteLine($"DVD is now Borrowed by: {Borrower}");
+
+        }
+        else
+        {
+            System.Console.WriteLine("Already the DVD is Borrowed");
+        }
+    }
+
+    public void Return()
+    {
+        if (Borrower != "")
+        {
+            Borrower = "";
+            System.Console.WriteLine($"{Title} by {Director} DVD Returned Successfuly!");
+        }
+        else
+            System.Console.WriteLine("DVD Not Borrowed");
+
+    }
+
+    public void Print()
+    {
+        System.Console.WriteLine($"DVD: {Title} of Director :{Director} of Length :{LengthInMinutes} is Borrowed by :{Borrower} for {LoanPeriod}");
+    }
+}
+
+public class CD: ILonable, IPrintable
+{
+    public string Artist {  get; set; } 
+    public string Title { get; set; }   
+    public int NumberOfTracks {  get; set; }
+
+    public int LoanPeriod { get; set; }
+
+    public string Borrower { get; set; }
+
+
+    public void Borrow(string b)
+    {
+        if (Borrower == "")
+        {
+            Borrower = b;
+            System.Console.WriteLine($"CD is now Borrowed by: {Borrower}");
+
+        }
+        else
+        {
+            System.Console.WriteLine("Already the CD is Borrowed");
+        }
+    }
+
+    public void Return()
+    {
+        if (Borrower != "")
+        {
+            Borrower = "";
+            System.Console.WriteLine($"{Title} by  Artist:{Artist} of {NumberOfTracks} Returned Successfuly!");
+        }
+        else
+            System.Console.WriteLine("CD Not Borrowed");
+
+    }
+
+    public void Print()
+    {
+        System.Console.WriteLine($"CD: {Title} of Artist :{Artist} of Tracks :{NumberOfTracks} is Borrowed by :{Borrower} for {LoanPeriod}");
+    }
+
+}
---
+++ LibraryAssignment/LibraryAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter Your Name:");
+        string name = Console.ReadLine();   
+
+        CD c = new CD();
+        c.Artist = "AR Rehman";
+        c.Title = "Song";
+        c.NumberOfTracks = 8;
+        c.LoanPeriod = 14;
+        c.Borrower = name;
+
+        c.Print();
+        c.Borrow(name);
+        c.Return();
+        c.Borrow("Sumit"); 
+
+        DVD d = new DVD();
+        d.LoanPeriod = 7;
+        d.Borrower = name; 
+        d.Director = "Sonu Nigam";
+        d.LengthInMinutes = 10;
+        d.Title = "Video Song";
+        d.Print();  
+
+        Book b = new Book();
+        b.Borrower = name;
+        b.Author = "J K Rowlin";
+        b.Title = "Rich Dad Poor Dad";
+        b.LoanPeriod = 21;
+        b.ISBN = "Affiliated by AION";
+        b.Print();
+
+    }
+}
