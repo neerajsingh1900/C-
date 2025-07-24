115e5d63a4ebd9ee394b834a709ce3b55a06f8ac
neerajsingh1900 <neeraj.singh@coditas.com>
2025-07-24 17:11:49 +0530

    Add files via upload

---
+++ Structure/Structure/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+struct Car
+{
+    public int Id;  
+    public string Name;
+
+    public Car()
+    {
+        Id = 0;
+        Name = "singh";
+    }
+    public void Show()
+    {
+        System.Console.WriteLine(Name);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Car c = new Car(); 
+        c.Show();
+    }
+}
