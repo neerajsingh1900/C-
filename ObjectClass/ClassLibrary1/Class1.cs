9ab882150e45260d735cdff92672cb5808b84d5b
neerajsingh1900 <neeraj.singh@coditas.com>
2025-07-24 17:11:05 +0530

    Add files via upload

---
+++ ObjectClass/ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        Student s = (Student)obj;
+
+        if (s.Id == this.Id && s.Name == this.Name) return true;
+        return false;   
+    }
+
+    public override string ToString()
+    {
+        return "Student Name:" + this.Name;
+    }
+
+}
---
+++ ObjectClass/ObjectClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Object o = new Student() { Id = 1, Name = "Neeraj" };
+
+        //only this method takes the parameter 
+        Console.WriteLine(o.Equals(new Student() { Id = 1, Name = "Neeraj" }));
+
+
+        Console.WriteLine(o.ToString());
+        Console.WriteLine(o.GetHashCode());
+
+        Console.WriteLine(o.GetType());
+
+
+    }
+}
