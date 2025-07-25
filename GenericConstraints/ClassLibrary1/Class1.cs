c8a02146ef7150e16531476523e85876a9a3f1b2
neerajsingh1900 <neeraj.singh@coditas.com>
2025-07-25 17:44:59 +0530

    Add files via upload

---
+++ GenericConstraints/ClassLibrary1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public abstract class Address
+{
+    public int pincode = 8;
+    public abstract void Display();
+
+
+}
+
+public class Student:Address
+{
+    public int pincode = 9;
+    public override void Display()
+    {
+        System.Console.WriteLine("Checking for ADDRESS");
+    }
+}
+
+public class Gen<T> where T : Address
+{
+    public T stu;
+
+    public void Show()
+    {
+       // stu = new Student();
+      //  System.Console.WriteLine(stu.pincode);
+        Address a = (Address)stu;
+        a = new Student();
+        System.Console.WriteLine(a.pincode);
+        a.Display();
+    }
+}
+
+public class Methodss
+{
+    public void Join<T>(T o)
+
+    {
+
+        if (o.GetType() == typeof(string))
+        {
+            System.Console.WriteLine("it is a string");
+        }
+        else if (o.GetType() == typeof(int))
+        {
+            System.Console.WriteLine("it is int");
+        }
+
+    }
+}
---
+++ GenericConstraints/GenericConstraints/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Gen<Student> g = new Gen<Student>();
+
+        g.Show();   
+
+        Methodss m = new Methodss();    
+        string name = "Test";
+        int age = 9;
+        m.Join<string>(name);
+        m.Join<int>(age);   
+    }
+}
