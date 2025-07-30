0d2c4ffb7fa5443890cf3f05ca844da6ddbd13f5
neerajsingh1900 <neeraj.singh@coditas.com>
2025-07-30 17:25:29 +0530

    Add files via upload

---
+++ GarbageCollector/ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+public class Database:IDisposable
+{
+    public Database()
+    {
+        System.Console.WriteLine("Database connected");
+    }
+
+    public void show()
+    {
+        Console.WriteLine("showing the connection");
+    }
+
+    public void Dispose()
+    {
+        System.Console.WriteLine("database connection closed");
+    }
+   
+}
---
+++ GarbageCollector/GarbageCollector/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+public class Program
+{
+
+        static void Do()
+    {
+        using Database db = new Database();
+        db.show();
+    }
+
+    public static void Main(string[] args)
+    {
+        Do();
+        using (Database d = new Database())
+        {
+            d.show();
+        }
+
+        System.Console.WriteLine("here we could see that" +
+            " it is called automatically!");
+    }
+}
