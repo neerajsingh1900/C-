e902346f6274082c3fe39785f2e1279775097c01
neerajsingh1900 <neeraj.singh@coditas.com>
2025-08-07 18:12:47 +0530

    Add files via upload

---
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Stack<int> s = new Stack<int>();
+        s.Push(8);
+        s.Pop();
+        s.Push(999);
+        s.Push(89);
+
+        int[] a = s.ToArray();
+
+        foreach(int i in a)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+        foreach (int i in s)
+        {
+            
+            Console.Write(i + " ");
+        }
+
+        Queue<string> q = new Queue<string>();
+
+        q.Enqueue("one");
+        q.Enqueue("two");
+        q.Enqueue("three");
+
+        Console.WriteLine();
+        foreach(string i in q)
+        {
+            Console.Write(i +" ");
+        }
+
+
+        string ele = q.Dequeue();  
+        Console.WriteLine(ele);
+
+        string e = q.Peek();
+
+        Console.WriteLine(e);
+
+        foreach (string i in q)
+        {
+            Console.Write(i + " ");
+        }
+
+    }
+}
