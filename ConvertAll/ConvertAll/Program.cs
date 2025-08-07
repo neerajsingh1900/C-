438cf0df5f342cd3461f2e63a604ed89e8fce319
neerajsingh1900 <neeraj.singh@coditas.com>
2025-08-07 18:11:59 +0530

    Add files via upload

---
+++ ConvertAll/ConvertAll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> l = new List<int>() { 1,2,3,4,5,6,7};
+
+      List<string> s =  l.ConvertAll(x => "Number :" + x);
+
+        foreach(string i in s)
+        {
+            Console.Write(i + " ");
+        }
+
+
+    }
+}
