02237f997c9d6fe250c616c168ece9a512cd4200
neerajsingh1900 <neeraj.singh@coditas.com>
2025-08-08 17:50:34 +0530

    Add files via upload

---
+++ EnumeratorExample/EnumeratorExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+public class Program
+{
+        public static IEnumerable<int> Meth()
+    {
+        Console.WriteLine("order for buiscuit");
+        Console.WriteLine("you have ordered:");
+        yield return 10;
+
+        Console.WriteLine("Want to add more");
+        Console.WriteLine("Enter the buiscuit");
+        yield return 20;    
+
+    }
+    public static void Main(string[] args)
+    {
+        // List<string> s
+        IEnumerable<string> s;
+        s = new List<string>() { "one", "two", "three", "four" };
+
+
+        // compensating foreach loop
+        IEnumerator<string> e = s.GetEnumerator();
+
+        e.Reset();
+        while (e.MoveNext())
+        {
+            Console.WriteLine(e.Current);
+        }
+
+    // Yield Return example
+    IEnumerator<int> a = Meth().GetEnumerator();
+
+        // we could use foreach this as it is of type ienumerable
+        //foreach(int i in Meth())
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+
+        // now doing manually
+        a.MoveNext();
+        Console.WriteLine(a.Current);
+        a.MoveNext() ;  
+        Console.WriteLine(a.Current);
+
+
+
+    }
+
+
+    
+}
