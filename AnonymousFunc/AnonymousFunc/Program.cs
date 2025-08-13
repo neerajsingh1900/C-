98c073b98bd5f514452159c1828ac74698248952
neerajsingh1900 <neeraj.singh@coditas.com>
2025-08-13 18:02:45 +0530

    Add files via upload

---
+++ AnonymousFunc/AnonymousFunc/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+public class Program
+{
+  public static void Main(string[] args)
+    {
+
+        Publisher p = new Publisher();
+        int a = 99;
+        p.Eve += delegate (int x, int y)
+        {
+            //using instance variable
+            Console.WriteLine(a+ x + y);
+        };
+
+        p.Eve += (a, b) =>
+        {
+            Console.WriteLine(a - b);
+        };
+
+        //p.Eve += (a, b) => a + b;
+
+        p.Show(11, 12);
+
+
+        // as a parameter
+        p.Showing(delegate(int x,int y)
+        {
+            Console.WriteLine(a - y-x);   
+        });
+
+    }
+}
---
+++ AnonymousFunc/ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    public delegate void Mydel(int x, int y);
+public class Publisher
+{
+    //  public event Mydel Eve;
+    //  public event Func<int, int, int> Eve;
+    public event Action<int, int> Eve;
+    public event Predicate<int> Ev;
+    public void Show(int x,int y)
+    {
+        Eve(x, y);
+    }
+
+
+    public void Showing(Mydel m)
+    {
+        m(13, 15);
+    }
+}
