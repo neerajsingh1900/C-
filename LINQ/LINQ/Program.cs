b25b236817d10ca43d48cf060c56717cb06d3072
neerajsingh1900 <neeraj.singh@coditas.com>
2025-08-19 17:31:08 +0530

    Add files via upload

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -14,19 +14,20 @@ public class Program
 {
     static void Print(IEnumerable<Employee> e)
     {
+        Console.WriteLine("in:");
         foreach (Employee i in e)
         {
             Console.WriteLine(i.Id + " " + i.Name + " " + i.Location + " " + i.Manager);
         }
     }
-
+    
     public static void Main(string[] args)
     {
         List<Employee> l = new List<Employee>()
         {
             new Employee(){Id=1,Name="Neeraj Singh" ,Manager ="Lokesh" ,Location = "Shimla"},
             new Employee(){Id=4,Name="Ankit Singh" ,Manager ="Sohan" ,Location = "Himachal"},
-            new Employee(){Id=2,Name="Sumit Singh" ,Manager ="Rajesh" ,Location = "Delhi"},
+            new Employee(){Id=2,Name="Sumit Singh" ,Manager ="Rajesh" ,Location = "Himachal"},
             new Employee(){Id=3,Name="Amit Singh" ,Manager ="Keshri" ,Location = "Mumbai"}
         };
 
@@ -51,6 +52,41 @@ public class Program
 
         e = l.OrderByDescending(e => e.Job).ThenBy(e => e.Id);
 
-        Print(e);       
+        //Print(e);
+
+
+        Employee em = l.First(i => i.Location == "Himachal");
+        //  Console.WriteLine(em.Id + " " + em.Name + " " + em.Manager);
+
+        //  em = l.First(i => i.Location == "New York");
+        //em = l.FirstOrDefault(i => i.Location == "New York");
+
+        //Console.WriteLine(em?.Id);
+
+        //em = l.Last(i => i.Location == "New York");
+        //Console.WriteLine(em.Id);
+
+        //em = l.LastOrDefault(i => i.Location == "New York");
+        //Console.WriteLine(em?.Id);  
+
+        // em = l.Where(i => i.Location == "New York").ElementAt(1);
+        //   e = l.Where(i => i.Location == "New York");
+        // Console.WriteLine(e.GetEnumerator());
+        // Print(e);
+
+
+        //em = l.Single(i=>i.Id ==1);  
+        //Console.WriteLine(em.Manager);
+
+        //em = l.Single(i => i.Id == 8);
+        //Console.WriteLine(em.Manager);  
+
+        //em = l.SingleOrDefault(i=> i.Id == 1);
+        //Console.WriteLine(em.Manager);
+
+        em = l.SingleOrDefault(i => i.Name == "Rahul");
+
+        Console.WriteLine(em?.Manager);  
+
     }
 }
