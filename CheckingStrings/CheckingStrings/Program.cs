b7de9108168a4d9886f3bd009facce2642519f4a
neerajsingh1900 <neeraj.singh@coditas.com>
2025-08-20 19:18:32 +0530

    Add files via upload

---
+++ CheckingStrings/CheckingStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    string a = "Neeraj";
+
+    bool check = a.StartsWith("Nee");
+
+        Console.WriteLine(check);
+
+        check = a.EndsWith("aj");
+        Console.WriteLine(check);
+
+        check = a.Contains("ra");
+        Console.WriteLine(check);
+
+        int index = a.IndexOf("e");
+           index = a.IndexOf("e",2);
+        index = a.IndexOf("d");
+        Console.WriteLine(index);
+
+        index = a.IndexOf('e', a.IndexOf('e') + 1);
+        Console.WriteLine(index);   
+
+
+
+
+
+    }
+
+}
