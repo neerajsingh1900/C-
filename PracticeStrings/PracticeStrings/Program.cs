74b27d2bbddcd2f7b8e82ab6f9755c26e17798fb
neerajsingh1900 <neeraj.singh@coditas.com>
2025-08-21 18:11:10 +0530

    Add files via upload

---
+++ PracticeStrings/PracticeStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string a = "developer@example.com";
+        char[] arr = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+
+      int cc =  a.Count(ch => Array.IndexOf(arr, ch)>=0);
+
+        Console.WriteLine("Vowel Count:"+ cc);
+        int count = 0;
+        for(int i = 0; i < a.Length; i++)
+        {
+
+            if (Array.IndexOf(arr, a[i])>=0)
+            count++;
+        }
+
+        Console.WriteLine("Vowel Count:" + count);
+    }
+}
