2bace9b06fd5ff0a74323c1239bcb66d054584db
neerajsingh1900 <neeraj.singh@coditas.com>
2025-08-21 18:12:09 +0530

    Add files via upload

---
+++ ModifyString/ModifyString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = "Neeraj";
+
+        string k = name.Insert(6, " Singh");
+        Console.WriteLine(k);
+
+        string hh = k.Remove(6, 6);
+        Console.WriteLine(hh);  
+    }
+}
