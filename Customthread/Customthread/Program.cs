e5c5458f1132f7ac2bc9dcade06447963f1087d4
neerajsingh1900 <neeraj.singh@coditas.com>
2025-08-27 18:24:45 +0530

    Add files via upload

---
+++ Customthread/Customthread/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+
+public class Counting
+{
+    public int Count { get; set; }  
+    public void Countup()
+    {
+        try
+        {
+            for (int i = 0; i <= Count; i++)
+            {
+                System.Console.ForegroundColor = ConsoleColor.Green;
+                  Console.Write(i + " ");
+            }
+        }
+        catch (ThreadInterruptedException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    public void Countdown()
+    {
+        for (int i = Count; i >= 0; i--)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(i + " ");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Counting c = new Counting();
+
+        c.Count = 100;
+
+        //creating parameterized function
+        ThreadStart th = c.Countup;
+
+
+        //creating parameterized function
+        ThreadStart tt = c.Countdown;
+
+        //creating thread for the function
+        Thread first = new Thread(th);
+
+
+        //creating thread for the function
+        Thread second = new Thread(tt);
+
+
+        //calling
+        first.Start();
+        second.Start();
+
+        Console.WriteLine("Main thread stopped");
+    }
+}
